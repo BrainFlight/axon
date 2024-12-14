8a402dd30ebeec1f56e8b71cb043be539122df5c
ammar siddiqui <ammar.siddiqui847@gmail.com>
2024-12-14 18:36:20 -0500

    updated text prompt service

--- platform/api/text_prompt.py
+++ platform/api/text_prompt.py
@@ -9,14 +9,15 @@ from services.text_prompt_service import text_prompt_service
 
 class TextPromptInput(BaseModel):
     model_name: str
-    prompt: str
+    prompt_format_name: str
     prompt_args: Optional[dict]
-    max_tokens: Optional[int]
-    temperature: Optional[float]
-    top_p: Optional[float]
-    frequency_penalty: Optional[float]
-    presence_penalty: Optional[float]
-    stop: Optional[List[str]]
+    rag_params: Optional[Dict[str, str]]
+    # max_tokens: Optional[int]
+    # temperature: Optional[float]
+    # top_p: Optional[float]
+    # frequency_penalty: Optional[float]
+    # presence_penalty: Optional[float]
+    # stop: Optional[List[str]]
     # load_to_cache: Optional[bool] = False
 
 class TextPromptV1PostResponse(BaseModel):
@@ -28,6 +29,12 @@ router = APIRouter()
 def text_prompt(body: TextPromptInput):
     """Text Prompt Endpoint."""
     # TODO: Validate input
-    response = text_prompt_service(body.prompt)
+
+    response = text_prompt_service(
+        body.model_name, 
+        body.prompt_format_name, 
+        body.prompt_args,
+        body.rag_params
+    )
 
     return TextPromptV1PostResponse(response={"Response": response})
