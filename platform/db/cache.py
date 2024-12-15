e06c2c4b27183e3dbae9ca2b58543a8cda38d15f
ammar siddiqui <ammar.siddiqui847@gmail.com>
2024-12-15 15:57:49 -0500

    new caching strategy for prompts

--- platform/db/cache.py
+++ platform/db/cache.py
@@ -19,7 +19,7 @@ class CacheClient:
     ):
         self.client = valkey.Valkey(host=host, port=port, db=db)
 
-    def set(self, key: str, value: str):
+    def set(self, key: str, value: str) -> bool:
         """
         Set value in cache.
 
@@ -27,7 +27,9 @@ class CacheClient:
             key: Key to set value for.
             value: Value to set.
         """
-        self.client.set(key, value)
+        if self.client.set(key, value):
+            return True
+        return False
 
     def get(self, key) -> str:
         """
@@ -41,14 +43,16 @@ class CacheClient:
         """
         return self.client.get(key)
     
-    def delete(self, key):
+    def delete(self, key) -> bool:
         """
         Delete key from cache.
 
         Args:
             key: Key to delete.
         """
-        return self.client.delete(key)
+        if self.client.delete(key):
+            return True
+        return False
     
     def exists(self, key) -> bool:
         """
@@ -73,7 +77,7 @@ class CacheClient:
         """
         return self.client.keys(pattern)
     
-    def hash_set(self, key: str, field: str, value: str):
+    def hash_set(self, key: str, field: str, value: str) -> bool:
         """
         Set value in hash.
 
@@ -82,7 +86,9 @@ class CacheClient:
             field: Field to set value for.
             value: Value to set.
         """
-        self.client.hset(key, field, value)
+        if self.client.hset(key, field, value):
+            return True
+        return False
 
     def hash_get(self, key: str, field: str) -> str:
         """
@@ -97,4 +103,14 @@ class CacheClient:
         """
         return self.client.hget(key, field)
     
-    
+    def hash_delete(self, key: str, field: str):
+        """
+        Delete field from hash.
+
+        Args:
+            key: Key of hash.
+            field: Field to delete.
+        """
+        if self.client.hdel(key, field):
+            return True
+        return False
